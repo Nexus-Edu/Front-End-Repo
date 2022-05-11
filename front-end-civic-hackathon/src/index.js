a65f902ec0b76e3339db46c767c9f47c70ede9b5
leo <cedillo47@gmail.com>
2022-05-11 18:34:03 -0400

    setting up index page to use router and context

--- front-end-civic-hackathon/src/index.js
+++ front-end-civic-hackathon/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom"
+import ContextProiver from "./components/context/ContextProvider"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ContextProiver>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </ContextProiver>
 );
 
 // If you want to start measuring performance in your app, pass a function
