a51e076cce35a039ac47dfd6c2c18e0f4c7895c1
leo <cedillo47@gmail.com>
2022-05-11 16:03:42 -0400

    set up context and routes

---
+++ front-end-civic-hackathon/src/context/ContextProvider.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import { useEffect } from "react";
+import Context from "./Context";
+
+function ContextProiver({children}){
+/// this is where we will add state that will be distribted though out the app. 
+
+    return (
+        <Context.Provider value={context}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export default ContextProiver
