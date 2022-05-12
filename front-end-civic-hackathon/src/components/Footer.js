f014eede70d3b55460bd6b11d11fd032f104c40a
leo <cedillo47@gmail.com>
2022-05-11 23:52:02 -0400

    made compomnents out od a vew of the js files

---
+++ front-end-civic-hackathon/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <div>
+            footer components 
+        </div>
+    )
+}
+
+export default Footer 
---
+++ front-end-civic-hackathon/src/components/home/Mission.js
@@ -0,0 +1,9 @@
+function Mission(){
+    return(
+        <div>
+            this will house our mission statment 
+        </div>
+    )
+}
+
+export default Mission
