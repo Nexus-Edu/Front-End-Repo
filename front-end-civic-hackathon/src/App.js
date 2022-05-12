ffe8d4869d69f4abed5d576cb5caa3869d51371b
Shevaugn Legall <91220146+shevaughn98@users.noreply.github.com>
2022-05-12 16:56:05 -0400

    footer update

--- front-end-civic-hackathon/src/App.js
+++ front-end-civic-hackathon/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/home/HomeContainer"
 import LoginSignUpPage from "./components/loginSignUp/LoginSignUpPage"
 import DiscussionBoard from "./components/discussionBoard/DiscussionBoard"
 import Comments from "./components/commentsPage/Comments"
+import Footer from './components/Footer';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/discussionBoard" element={<DiscussionBoard/>}/>
         <Route path="/discussionBoard/:id" element={<Comments/>}/>
       </Routes>
+      <Footer />
     </div>
   );
 }
