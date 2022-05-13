4f005758bcef1de749363a90185cbd4e5f41ab44
leo <cedillo47@gmail.com>
2022-05-13 12:00:12 -0400

    fixing bug

--- front-end-civic-hackathon/src/App.js
+++ front-end-civic-hackathon/src/App.js
@@ -6,18 +6,20 @@ import LoginSignUpPage from "./components/loginSignUp/LoginSignUpPage"
 import DiscussionBoard from "./components/discussionBoard/DiscussionBoard"
 import Comments from "./components/commentsPage/Comments"
 import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/loginSignUp" element={<LoginSignUpPage/>}/>
         <Route path="/discussionBoard" element={<DiscussionBoard/>}/>
         <Route path="/discussionBoard/:id" element={<Comments/>}/>
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
