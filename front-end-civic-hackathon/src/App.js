95044caa8b817a73e006d87f6e12c06db765e643
leo <cedillo47@gmail.com>
2022-05-12 22:54:41 -0400

    testing app

--- front-end-civic-hackathon/src/App.js
+++ front-end-civic-hackathon/src/App.js
@@ -1,27 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from "./components/home/HomeContainer"
 import LoginSignUpPage from "./components/loginSignUp/LoginSignUpPage"
 import DiscussionBoard from "./components/discussionBoard/DiscussionBoard"
 import Comments from "./components/commentsPage/Comments"
-import NavBar from "./components/NavBar"
- 
- 
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
-      <header>
-        <NavBar/>
-      </header>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/loginSignUp" element={<LoginSignUpPage/>}/>
         <Route path="/discussionBoard" element={<DiscussionBoard/>}/>
         <Route path="/discussionBoard/:id" element={<Comments/>}/>
       </Routes>
-     
+      <Footer />
     </div>
   );
 }
- 
+
 export default App;
