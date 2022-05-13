7eb4263e2946806481b03824417de52422fdcc25
leo <cedillo47@gmail.com>
2022-05-12 21:26:35 -0400

    compleated navbar

--- front-end-civic-hackathon/src/components/NavBar.js
+++ front-end-civic-hackathon/src/components/NavBar.js
@@ -1,8 +1,25 @@
+import React from "react"
+import { Link } from "react-router-dom";
+
+
 function NavBar(){
     return(
-        <div>
-            navBar components 
-        </div>
+<ul class="nav">
+
+  <li class="nav-item">
+    {/* <a class="nav-link active" aria-current="page" href="#">Active</a> */}
+    <Link to="/loginSignUp" class="nav-link active" >loginSignUp</Link>
+  </li>
+  <li class="nav-item">
+    <Link to ="/discussionBoard" class="nav-link active">DiscussionBoard</Link>
+  </li>
+  <li class="nav-item">
+    <Link to ="/" class="nav-link" >Home</Link>
+  </li>
+  {/* <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li> */}
+</ul>
     )
 }
 
