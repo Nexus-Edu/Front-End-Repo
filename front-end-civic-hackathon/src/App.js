18c3b0bd5d5c048b779d7e238e7cceeb569084ea
leo <cedillo47@gmail.com>
2022-05-15 15:43:35 -0400

    undid hiding footer before merge

--- front-end-civic-hackathon/src/App.js
+++ front-end-civic-hackathon/src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route path="/discussionBoard/:id" element={<Comments/>}/>
       </Routes>
       <footer>
-        {/* <Footer/> */}
+        <Footer/>
       </footer>
     </div>
   );
