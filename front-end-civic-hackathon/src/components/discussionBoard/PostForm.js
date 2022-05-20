4b47555cceecdac32befe2f6d92591bbacd87152
leo <cedillo47@gmail.com>
2022-05-20 12:17:00 -0400

    fixedbug

--- front-end-civic-hackathon/src/components/discussionBoard/PostForm.js
+++ front-end-civic-hackathon/src/components/discussionBoard/PostForm.js
@@ -7,22 +7,18 @@ what is happening ? i am getting the new post but only once and thats it, figure
 */
 
 export default function PostForm() {
-    const { newPost, setNewPost, post, setPost} = useContext(Context)
-    const [sent, setSent] = useState(1);
-    const [newP , setNewP] = useState([]); 
-
+    const { newPost, setNewPost, post, setPost } = useContext(Context)
 
     const user = {
         user_id: 10
     }
 
-    
+
     const [state, setState] = useState({
-        descriptionInput: "",
-        hashTagInput: "",
+        message: "",
+        hashtag: "",
     })
-    
-    const body = Object.assign(user, newPost);
+
 
     const handleChange = e => {
         setState({
@@ -31,55 +27,56 @@ export default function PostForm() {
         })
     }
 
-    useEffect(()=>{
-            async function sendPost(){
-               const res = await fetch('http://localhost:5000/board/post', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(body)
-            })
-            const data = await res.json()
-            console.log(data.message)
-            // if(data.message !== "Please enter a message or hashtag"){
-            setNewP(data)    
-        }
-            sendPost()
-            console.log(newP)
-    }, [sent])
+
+    async function sendPost() {
+        const body = Object.assign(user, state);
+
+
+
+
+        console.log(body)
+        const res = await fetch('http://localhost:5000/board/post', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+        })
+
+        const resTwo = await fetch('http://localhost:5000/board')
+        const data = await resTwo.json()
+        setPost(data)
+    }
+
 
     return (
         <form onSubmit={(e) => {
             e.preventDefault()
-            setNewPost({
-                message: state.descriptionInput,
-                hashtag: state.hashTagInput
-            })
-            setSent(true)
             setState({
-                descriptionInput: "",
-                hashTagInput: "",
+                message: "",
+                hashtag: "",
             })
-
         }}>
             <label htmlFor="description">
                 Description:
             </label>
-            <textarea name="descriptionInput" value={state.descriptionInput} type="text" rows="4" cols="60" maxlength="150" placeholder="up to 150 char" onChange={handleChange}>
+            <textarea name="message" value={state.message} type="text" rows="4" cols="60" maxlength="150" placeholder="up to 150 char" onChange={handleChange}>
             </textarea>
 
             <label>
                 Hashtags:
             </label>
 
-            <input type="text" value={state.hashTagInput} name="hashTagInput" placeholder="ex: #Tech #Manhattan" onChange={handleChange} />
+            <input type="text" value={state.hashtag} name="hashtag" placeholder="ex: #Tech #Manhattan" onChange={handleChange} />
 
             <br></br>
             <br></br>
             <div class="modal-footer">
-              <button type="submit" class="btn btn-secondary" value="make a post" data-bs-dismiss="modal">make a post</button>
-              <button type="sumbit" class="btn btn-secondary" data-bs-dismiss="modal" onClick={ e =>{
-                   setSent(1)
-              }}>Cancel</button>
+                <button type="submit" class="btn btn-secondary" value="make a post" data-bs-dismiss="modal" onClick={sendPost}>make a post</button>
+                <button type="sumbit" class="btn btn-secondary" data-bs-dismiss="modal" onClick={e => {
+                    setState({
+                        message: "",
+                        hashtag: "",
+                    })
+                }}>Cancel</button>
             </div>
         </form>
     )
--- front-end-civic-hackathon/src/components/discussionBoard/PostUl.js
+++ front-end-civic-hackathon/src/components/discussionBoard/PostUl.js
@@ -1,24 +1,16 @@
 import Post from './Post'
-import {Link} from "react-router-dom"
 import Context from "../../context/Context"
 import { useContext, useEffect, useState } from "react"
 import { Button } from "reactstrap"
 
 
-
 function PostUl(props){
-    const { array } = props
-    const {userInfo} = useContext(Context)
-    console.log(array)
+    // const { array } = props
+    const { post} = useContext(Context)
     return(
         <ul>{
-            array.map((post) => <li  key={post.id}>
-                <Link to={`/discussionBoard/${post.id}`}> 
-                <Post name={post.name} profilePic={post.profile_pic} message={post.message} hashtag={post.hashtag} date={post.date} username={post.username}/>
-                </Link>
-                {userInfo !== post.username ? <></>: <Button onClick={()=>{
-                    
-                }}>delete</Button>} 
+            post.map((post) => <li  key={post.id}>
+                <Post name={post.name} profilePic={post.profile_pic} message={post.message} hashtag={post.hashtag} date={post.date} username={post.username} id={post.id}/>
                 </li>)
         }</ul>
     )
