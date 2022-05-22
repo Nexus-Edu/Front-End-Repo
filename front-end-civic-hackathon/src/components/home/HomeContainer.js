06e21794857399dd96fcda32e6bd9c8c4fc403fa
leo <cedillo47@gmail.com>
2022-05-22 15:23:15 -0400

    was able to set user via log in

--- front-end-civic-hackathon/src/components/home/HomeContainer.js
+++ front-end-civic-hackathon/src/components/home/HomeContainer.js
@@ -1,8 +1,13 @@
 import Mission from "./Mission"
- 
+import Context from "../../context/Context" 
+import { useContext } from "react"
+
 function HomeContainer(){
+    const {userInfo} = useContext(Context)
+    console.log(userInfo)
     return(
         <div>
+            {!userInfo ? <></> : <h1>welcome : {userInfo.username}</h1>}
             <Mission/>
         </div>
     )
--- front-end-civic-hackathon/src/components/loginSignUp/LoginSignUpPage.js
+++ front-end-civic-hackathon/src/components/loginSignUp/LoginSignUpPage.js
@@ -1,5 +1,7 @@
 import Login from "./Login"
 import Signup from "./Signup"
+import Context from "../../context/Context"
+import React, { useState, useEffect, useContext } from 'react'
 
 function loginSignUp(){
     return(
