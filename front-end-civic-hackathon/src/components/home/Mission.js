725d9d2b06bba7c89039e9b596e4afd33b229b02
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2022-05-24 13:51:33 -0400

    Added Grommet w/ Routed Button

--- front-end-civic-hackathon/src/components/home/Mission.js
+++ front-end-civic-hackathon/src/components/home/Mission.js
@@ -1,6 +1,9 @@
 // import Mission from "./Mission"
+import { Box, RoutedButton as GrommetRoutedButton, Text } from 'grommet';
  
 function Mission(){
+
+    
     return(
         <div>
             <h1 class="mission-heading">Welcome to NexusEdu</h1>
@@ -10,6 +13,8 @@ function Mission(){
             Inform the youth of underprivileged areas of free educational opportunities by providing a platform that allows people to share free educational alternatives to college.
             </p>
             <img src="http://indigoyogadayton.com/wp-content/uploads/2013/10/gratitude.jpg" width="40%"class="rounded mx-auto d-block" alt="..."></img>
+            <br />
+            <GrommetRoutedButton label="Sign Up" path="/loginSignUp" />
             </div>
         </div>
     )
