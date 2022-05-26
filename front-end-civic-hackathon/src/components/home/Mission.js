480e995baafb60b06c69a5272b9e3f030b0c86ee
Shevaugn Legall <91220146+shevaughn98@users.noreply.github.com>
2022-05-26 12:22:26 -0400

    update

--- front-end-civic-hackathon/src/components/home/Mission.js
+++ front-end-civic-hackathon/src/components/home/Mission.js
@@ -8,11 +8,11 @@ function Mission(){
     return(
         <section className='flex-container'>
             <section className='banner'>
-                <h1>Nexus Edu </h1>
+                <h1 className='home-title'>Nexus Edu </h1>
             <img src='https://lifereinvented.com/wp-content/uploads/2019/08/blog-4-1024x714.jpg'/>
             </section>
 
-            <sction className='flex-box'>
+            <section className='flex-box'>
                 <section className='flex-box1'>
                     <section>
                         <img className='home-image' src='https://cdn-icons-png.flaticon.com/512/3867/3867730.png'/>
@@ -32,8 +32,17 @@ function Mission(){
                     <img className='home-image' src='https://cdn-icons-png.flaticon.com/512/3867/3867730.png'/>
                 </section>
                  </section>
-            </sction>
-            <GrommetRoutedButton label="Sign Up" path="/loginSignUp" />
+            </section>
+            <section className='flex-box3'>
+                    <section>
+                        <img className='home-image' src='https://cdn-icons-png.flaticon.com/512/3867/3867730.png'/>
+                    </section>
+                    <section className=''>
+                        <h2>sub heading</h2>
+                        <p>body</p>
+                    </section>
+                </section>
+            {/* <GrommetRoutedButton label="Sign Up" path="/loginSignUp" /> */}
         </section>
         
     )
